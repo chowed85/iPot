3763d9dfe3773cbc9513c58859be679d25b197e6
chowed85 <55556986+chowed85@users.noreply.github.com>
2019-11-07 17:42:33 -0500

    Add files via upload
    This has some preliminary changes for turning the json sender we made in the lab into a communications program we can use

---
+++ jsonSender-Revised.py
@@ -0,0 +1,44 @@
+# Source: https://pymotw.com/2/socket/udp.html
+
+import socket, sys, time, json
+
+host = sys.argv[1]
+textport = sys.argv[2]
+ptype = sys.argv[3]
+wvalue = sys.argv[4]
+tvalue = sys.argv[5]
+time = sys.argv[6]
+name = sys.argv[7]
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = int(textport)
+server_address = (host, port)
+
+
+x = {
+   "type": ptype,
+   "humidity": wvalue,
+   "temp": tvalue,
+   "time" : time,
+   "name" : name
+}
+
+# convert into JSON:
+y = json.dumps(x)
+        
+
+#    s.sendall(data.encode('utf-8'))
+s.sendto(y.encode('utf-8'), server_address)
+i = 1
+while i<=3:
+            buf, address = d.recvfrom(port)
+            if not len(buf):
+                        break
+            g=json.loads(buf)
+            if y["type"] == 6:
+                        break
+            break
+
+s.shutdown(1)
+
