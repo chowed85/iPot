3c35287825c0e2cce880fcabfeec85fa025d43c9
Zahmed21 <40894862+Zahmed21@users.noreply.github.com>
2019-11-16 17:15:48 -0500

    Initialize iPot Activity

--- iPot2/app/src/main/java/com/example/ipot/MainActivity.java
+++ iPot2/app/src/main/java/com/example/ipot/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.ipot;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button initPot;
+    Button fetchData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Main Page");
+
+        initPot = (Button)findViewById(R.id.setPoint);
+
+        initPot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSetPointActivity();
+            }
+        });
+    }
+
+    public void openSetPointActivity(){
+        Intent intent = new Intent(this, SetPointPot.class);
+        startActivity(intent);
     }
 }
---
+++ iPot2/app/src/main/java/com/example/ipot/SetPointPot.java
@@ -0,0 +1,14 @@
+package com.example.ipot;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SetPointPot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_point_pot);
+    }
+}
