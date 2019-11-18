cad7e44487c58e7e1eb3bc942e95d14294ffd9ad
chowed85 <55556986+chowed85@users.noreply.github.com>
2019-11-18 11:23:34 -0500

    Update jsonReceiver-Revised.py
    added comments

--- jsonReceiver-Revised.py
+++ jsonReceiver-Revised.py
@@ -1,16 +1,18 @@
 import socket, sys, time,json
-#control variables
+#control variables used for configuration
 Wmin = 700
 hightemp = 60
 lowtemp = -10
 Wmax = 0
 dataValid = False
 
+#setting up input socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = ('localhost', port)
 s.bind(server_address)
 
+#setting up output socket
 d= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 p = 1007
 sa = ('localhost', p)
@@ -27,7 +29,8 @@ def respond(acktype):
 while True:
 
     print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
-
+    
+    #loads the info from the input port and load the json into variables
     buf, address = s.recvfrom(port)
     y = json.loads(buf)
     if not len(buf):
@@ -38,21 +41,26 @@ while True:
     tvalue = y['temp']
     time = y['time']
     name = y['name']
+    
+    #pot creation checking for type 7 or type 8 errors
     if ptype == 1:
-        #pot creation
         if wvalue == None or tvalue == None or time == None or name ==None:
             respond(7)
         if wvalue <Wmin or time < 0:
             respond(8)
         else:
             respond(6)
+    
+    #Request for info on a Pot checking for type 7 or type 8 errors
     elif ptype == 2:
         if name == None:
             respond(7)
         #have if statment for if name is not in database
         #   respond(8)
         else:
-            respond(6)        
+            respond(6) 
+            
+    #Stored sensor log from database incomplete packet checking for type 7 or type 8 errors
     elif ptype==3:
         if wvalue == None or tvalue == None or time == None or name ==None:
             respond(7)
@@ -60,6 +68,8 @@ while True:
             respond(8)
         else:
             respond(6)
+            
+    #Stored sensor log from database end packet checking for type 7 or type 8 errors
     elif ptype==4:
         if wvalue == None or tvalue == None or time == None or name ==None:
             respond(7)
@@ -67,6 +77,7 @@ while True:
             respond(8)        
         else:
             respond(6)
+    #Packet containing sensor data from arduino to database checking for type 7 or type 8 errors        
     elif ptype == 5:
         if wvalue == None or tvalue == None or time == None or name ==None:
             respond(7)
@@ -74,6 +85,7 @@ while True:
             respond(8)        
         else:
             respond(6)
+    #anything else clause
     else:
         respond(7)
     if dataValid:
