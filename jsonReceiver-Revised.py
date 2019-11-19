eef51eeba9a7f6845ec6b3e1880cf7ee5ad29ca8
chowed85 <55556986+chowed85@users.noreply.github.com>
2019-11-18 20:11:41 -0500

    Update jsonReceiver-Revised.py
    Due to similar conditions in testing type 3-5 packets, collapsed if statements together, renamed variables to consistent naming scheme, updated port protocol to bypass errors

--- jsonReceiver-Revised.py
+++ jsonReceiver-Revised.py
@@ -1,10 +1,13 @@
 import socket, sys, time,json
 #control variables used for configuration
-Wmin = 700
-hightemp = 60
-lowtemp = -10
-Wmax = 0
+wMin = 700
+highTemp = 60
+lowTemp = -10
+wMax = 0
 dataValid = False
+
+textport = sys.argv[1]
+
 
 #setting up input socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,7 +19,7 @@ s.bind(server_address)
 d= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 p = 1007
 sa = ('localhost', p)
-d.bind(sa)
+
 
 #method for sending response to sender program, acktype is the int code used for responses
 def respond(acktype):
@@ -36,23 +39,23 @@ while True:
     if not len(buf):
         break
     print ("Received %s bytes from %s %s: " % (len(buf), address, y ))
-    ptype = y['type']
-    wvalue = y['humidity']
-    tvalue = y['temp']
+    pType = y['type']
+    wValue = y['humidity']
+    tValue = y['temp']
     time = y['time']
     name = y['name']
     
     #pot creation checking for type 7 or type 8 errors
-    if ptype == 1:
-        if wvalue == None or tvalue == None or time == None or name ==None:
+    if pType == 1:
+        if wValue == None or time == None or name ==None:
             respond(7)
-        if wvalue <Wmin or time < 0:
+        if wValue >wMin or time < 0:
             respond(8)
         else:
             respond(6)
     
     #Request for info on a Pot checking for type 7 or type 8 errors
-    elif ptype == 2:
+    elif pType == 2:
         if name == None:
             respond(7)
         #have if statment for if name is not in database
@@ -61,35 +64,19 @@ while True:
             respond(6) 
             
     #Stored sensor log from database incomplete packet checking for type 7 or type 8 errors
-    elif ptype==3:
-        if wvalue == None or tvalue == None or time == None or name ==None:
+    elif pType==3 or pType ==4 or pType == 5:
+        if wValue == None or tValue == None or time == None or name ==None:
             respond(7)
-        elif lowtemp> tvalue or tvalue > highvalue or wvalue<Wmax or wvalue>Wmin:
+        elif lowTemp> tValue or tValue > highTemp or wValue<wMax or wValue>wMin:
             respond(8)
-        else:
-            respond(6)
-            
-    #Stored sensor log from database end packet checking for type 7 or type 8 errors
-    elif ptype==4:
-        if wvalue == None or tvalue == None or time == None or name ==None:
-            respond(7)
-        elif lowtemp> tvalue or tvalue > highvalue or wvalue<Wmax or wvalue>Wmin:
-            respond(8)        
-        else:
-            respond(6)
-    #Packet containing sensor data from arduino to database checking for type 7 or type 8 errors        
-    elif ptype == 5:
-        if wvalue == None or tvalue == None or time == None or name ==None:
-            respond(7)
-        elif lowtemp> tvalue or tvalue > highvalue or wvalue<Wmax or wvalue>Wmin:
-            respond(8)        
         else:
             respond(6)
     #anything else clause
     else:
         respond(7)
     if dataValid:
-        print("type: %s, humidity: %s, temp: %s, time: %s, name: %s",ptype, wvalue, tvalue, time, name)
+        print("type: %s, humidity: %s, temp: %s, time: %s, name: %s",pType, wValue, tValue, time, name)
+        dataValid = false
     
         
         
