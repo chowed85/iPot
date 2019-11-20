97595e2cb660f51e1c759f0097c7fe206678ea38
Kevin9921 <55557426+Kevin9921@users.noreply.github.com>
2019-11-20 16:23:18 -0500

    Add files via upload

--- DatabaseSender3.py
+++ DatabaseSender3.py
@@ -3,7 +3,7 @@ import unittest
 import datetime
 from sqlite3 import Error
  
- 
+#creates the connecction with the database
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
     conn = None
@@ -18,61 +18,88 @@ def create_connection(db_file):
  
     return conn
 
-#adds in a rwo to table
-def addData(cursor,name,waterSense, tempSense, time):
-    cursor.execute("INSERT INTO test VALUES(?, ?, ?, ?) ",(name, waterSense, tempSense, time))
+#adds in a row of data to the database
+def addData(cursor, name, waterSense, tempSense, time):
+    cursor.execute("INSERT INTO test VALUES (?, ?, ?, ?) ", (name, waterSense, tempSense, time))
 
-#deletes all table data
+#deletes all database data
 def deleteAll(cursor):
     cursor.execute('DELETE FROM test')
 
-#gets a column of table
-def getColumn(cursor):
-    cursor.execute("""SELECT DISTINCT time FROM test""")
-    
+#gets a row of the database
+def getRow(cursor, row):
+    cursor.execute("SELECT * FROM test")
     data = cursor.fetchall()
-    print(data[0])
+    return data[row]
     
 
-#gets all data from table  
+#gets all data from database 
 def getAllData(cursor):
     cursorObj.execute('SELECT * FROM test')
     data = cursorObj.fetchall()
     print(data)
-    
+
+#deletes specific rows of data from the database 
+def deleteRow(cursor, name):
+    cursor.execute("DELETE FROM test WHERE name like '" + name + "'")
 
  
-def main():
-    conn = create_connection(r"/Users/owner/Documents/testDb.db")
+#def main():
+    #conn = create_connection(r"/Users/owner/Documents/testDb.db")
+    #cursorObj = conn.cursor()
 
-   
-    
-    cursorObj = conn.cursor()
     #addData(cursorObj, "John", 599, 434, date)
-
-    getColumn(cursorObj)
+    #getColumn(cursorObj)
     #getAllData(cursorObj)
-    deleteAll(cursorObj)
+    #deleteAll(cursorObj)
     
-    conn.commit()
+    #conn.commit()
 
 class tester(unittest.TestCase):
+
+    #instance variables for testing
     name = "Michael"
     waterSense = 550
     tempSense = 200
     time = datetime.date(2019, 4, 13)
 
+    #connects to database
     conn = create_connection(r"/Users/owner/Documents/testDb.db")
     cursorObj = conn.cursor()
 
+    #tests the addData method to see if data can be added into the database
     def testAddData(self):
         addData(self.cursorObj, self.name, self.waterSense, self.tempSense, self.time)
-        
-        self.cursorObj.execute("""SELECT DISTINCT name FROM test WHERE waterSense = 550""")
+        self.cursorObj.execute("""SELECT DISTINCT name FROM test""")
         data = self.cursorObj.fetchall()
-        print(data[0])
-        #self.assertEqual(self.name, data[0])
-        #self.conn.commit()
+        self.assertEqual(self.name, data[0][0])
+
+    #tests the DeleteAll method to see if it deletes all the data from the database
+    def testDeletealldata(self):
+        addData(self.cursorObj, self.name, self.waterSense, self.tempSense, self.time)
+        deleteAll(self.cursorObj)
+        self.cursorObj.execute("""SELECT * FROM test""")
+        data = self.cursorObj.fetchall()
+        self.assertEqual(0, len(data))
+
+    #tests the getRow method to see if you can get the correct row of data from the database
+    def testRowData(self):
+        addData(self.cursorObj, self.name, self.waterSense, self.tempSense, self.time)
+        data = getRow(self.cursorObj,0)
+        self.assertEqual(self.name, data[0])
+        self.assertEqual(self.waterSense, data[1])
+        self.assertEqual(self.tempSense, data[2])
+        self.assertEqual(str(self.time), data[3])
+
+    #tests the deleteRow method to see is you can delete specific rows of data from the database
+    def testDeleteRow(self):
+        addData(self.cursorObj, self.name, self.waterSense, self.tempSense, self.time)
+        deleteRow(self.cursorObj, self.name)
+        self.cursorObj.execute("SELECT * FROM test WHERE name like'" + self.name + "'")
+        data = self.cursorObj.fetchall()
+        self.assertEqual([], data)
+        
+        
 if __name__ == '__main__':
     unittest.main() 
         
