6644637c4e2eea81c360d71e40705a3dfd42ede4
chowed85 <55556986+chowed85@users.noreply.github.com>
2019-11-27 15:04:49 -0500

    Update jsonSender-Revised.py
    updated code to have second socket for listening for outputs

--- jsonSender-Revised.py
+++ jsonSender-Revised.py
@@ -3,18 +3,25 @@
 import socket, sys, time, json
 
 host = sys.argv[1]
-textport = sys.argv[2]
-ptype = sys.argv[3]
-wvalue = sys.argv[4]
-tvalue = sys.argv[5]
-time = sys.argv[6]
-name = sys.argv[7]
+ptype = sys.argv[2]
+wvalue = sys.argv[3]
+tvalue = sys.argv[4]
+time = sys.argv[5]
+name = sys.argv[6]
 
 
+
+#setting up input socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
-server_address = (host, port)
+server_address = (host, 1006)
 
+
+#setting up output socket
+d= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+p = 1007
+sa = (host, p)
+d.bind(sa)
 
 x = {
    "type": ptype,
@@ -36,21 +43,35 @@ while i<=3:
    if not len(buf):
       time.delay(10)
       buf, address = d.recvfrom(port)
-   if not len(buf):  
+   if not len(buf):
+      i = i+1
    else:
       g=json.loads(buf)
       if g["type"] == 6:
          break
       elif g["type"] == 7 or g["type"] == 8:
-         #repack data from args into x here
+         x = {
+            "type": ptype,
+            "humidity": wvalue,
+            "temp": tvalue,
+            "time" : time,
+            "name" : name
+         }
+
+         # convert into JSON:
+         y = json.dumps(x)
+         s.sendto(y.encode('utf-8'), server_address)
+         i = i+1
       else:
          s.sendto(y.encode('utf-8'), server_address)
-   i+=i
+
 
 if i==4:
    print("there was a type " + g["type"] + "error with the packet")
+else:
+   print("Packet Successful")
 
    
 
-s.shutdown(1)
-
+d.shutdown(1)
+d.close()
