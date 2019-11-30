d12cd6aa8e832e762d048dd70f7aaaef98e30c54
Zahmed21 <40894862+Zahmed21@users.noreply.github.com>
2019-11-29 22:11:27 -0500

    Sending set point data to the Front End Pi

--- iPot2/app/src/main/java/com/example/ipot/SetPointPot.java
+++ iPot2/app/src/main/java/com/example/ipot/SetPointPot.java
@@ -1,5 +1,7 @@
 package com.example.ipot;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,23 +12,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+
 public class SetPointPot extends AppCompatActivity {
 
-    String name,moisture,interval;
-    EditText plantName,Sens,Tint;
-    Socket socket;
+    String name, moisture, interval;
+    EditText plantName, Sens, Tint;
+    DatagramSocket socket = null;
 
-    int portnum = 8018;
+
+    int portnum = 8008;
     InetAddress IPADDRESS;
-    String myIP = "192.168.0.21";
+    //    String myIP = "192.168.43.77";
+    //String myIP = "192.168.43.32";
+    String myIP = "192.168.1.101";
+
+
     Button CREATE;
     JSONObject NEWPOT;//Packet sent to create new pot
 
@@ -35,7 +42,7 @@ public class SetPointPot extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_point_pot);
 
-        plantName = (EditText) findViewById(R.id.plantName) ;
+        plantName = (EditText) findViewById(R.id.plantName);
         Sens = (EditText) findViewById(R.id.setMoist);
         Tint = (EditText) findViewById(R.id.setTint);
         CREATE = (Button) findViewById(R.id.createPot);
@@ -44,6 +51,10 @@ public class SetPointPot extends AppCompatActivity {
         moisture = Sens.getText().toString();
         interval = Tint.getText().toString();
 
+        CREATE.setOnClickListener(buttonConnectOnClick);
+        //udpClientHandler = new UdpClientHandler(this);
+    }
+/*
         //Create an IPAddress of the Front end RPi
         try{
             IPADDRESS = InetAddress.getByName(myIP);
@@ -51,72 +62,77 @@ public class SetPointPot extends AppCompatActivity {
         }catch(UnknownHostException e){
             e.printStackTrace();
         }
+        */
 
-        CREATE.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-              NEWPOT = new JSONObject();
-              try{
-                    NEWPOT.put("PotName", name);
-                    NEWPOT.put("MoistureSet", moisture);
-                    NEWPOT.put("TimeInterval", interval);
+    View.OnClickListener buttonConnectOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-                  Thread potSender = new Thread(new Sender(IPADDRESS,portnum, NEWPOT.toString()));
-                  potSender.start();
-              }catch (JSONException e){
-                    e.printStackTrace();
-                }
+            Sender setpointsend = new Sender();
+            setpointsend.execute();
 
+        }
+    };
+
+    class Sender extends AsyncTask<Void, Void, Void> {
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            //Create an IPAddress of the Front end RPi
+            try {
+                IPADDRESS = InetAddress.getByName(myIP);
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
             }
-        });
 
-    }
+            NEWPOT = new JSONObject();
+            name = plantName.getText().toString();
+            moisture = Sens.getText().toString();
+            interval = Tint.getText().toString();
+            try {
+                NEWPOT.put("type", 1);
+                NEWPOT.put("humidity", moisture);
+                NEWPOT.put("temp", null);
+                NEWPOT.put("time", interval);
+                NEWPOT.put("name", name);
+                String NewPot1 = NEWPOT.toString();
+                System.out.println("The message sent is:" + NewPot1);
+                byte[] data = NewPot1.getBytes();
+                System.out.println("The data byte array is created");
 
-    private class Sender implements Runnable {
-        int port;
-        String message;
-        InetAddress ip;
-        String messageReceived;
+                //while (true) {
 
-        /**
-         * Construct
-         *
-         * @param ip;
-         * @param port;
-         * @param message;
-         */
+                    //DatagramSocket socket = null;
+                    try {
+                        socket = new DatagramSocket(8008);
+                        System.out.println("socket has been created");
 
-        public Sender(InetAddress ip, int port, String message) {
-            this.port = port;
-            this.ip = ip;
-            this.message = message;
+                    } catch (SocketException e) {
+                        e.printStackTrace();
+                        System.out.println("failed to create socket");
+                    }
+
+                    DatagramPacket packet = new DatagramPacket(data, data.length, IPADDRESS, portnum);
+                System.out.println("dpacket has been created");
+                    try {
+                        System.out.println("we have entered in the send packet trycatch");
+                        socket.send(packet);
+                        System.out.println("send done");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+            socket.close();
+
+              //  }//end while(true)
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+           // socket.close();
+            return null;
         }
 
-        @Override
-        public void run() {
-            try {
-
-                try{socket = new Socket(ip, port);
-            } catch (SocketException e) {
-                e.printStackTrace();
-            }
-//initiate output stream
-            DataOutputStream cmdOut = new DataOutputStream(socket.getOutputStream());
-
-            while (true) {
-                cmdOut.writeBytes(message); //Convert the command to bytes.
-                System.out.println("Packet sent:   " + message);
-
-                //Receiving ACK from the input stream
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                messageReceived = in.readLine();
-
-                socket.close();
-            }//end while(true)
-        }catch(Exception e){
-                e.printStackTrace();
-            }//end catch
-
-        }//end run
-    }//end Sender class
-}//end setPointPot class
+    }//end setPointPot class
+}
